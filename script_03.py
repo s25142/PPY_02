e1993618b44d0c14f057ebc074486c8be0f2fbcc
s25142 <s25142@pjwstk.edu.pl>
2023-03-15 21:55:13 +0100

    utworzenie bazy pytań

---
+++ script_03.py
@@ -0,0 +1,51 @@
+"odpowiedź: "
+
+"pytanie: "
+
+"Jakie masz na imie i nazwisko ?"
+
+
+"1.Najczęstszym sposobem spędzania wolnego czasu jest dla Ciebie: "
+    "oglądanie telewizji/filmów/seriali "
+    "słuchanie muzyki"
+    "podróżowanie"
+
+    "2. W jakich okolicznościach czytasz książki najczęściej ?"
+    "podczas podróży"
+    " w ogóle nie czytam"
+    "w czasie wolnym"
+
+
+    "3. Po jakie gatunki książek sięgasz najczęściej ?"
+    " horrory"
+    "naukowe"
+    "fantastyke"
+
+    "4. W jakim języku książki czytasz? "
+    "polskim"
+    "angielskim"
+    "rosyjskim"
+
+    "5. W jaki sposób najczęściej zdobywasz interesujące Cię tytuły?"
+    "wypożyczam w bibliotece stacjonarnej"
+    "kupuję przez internet"
+    "kupuję w księgarni stacjonarnej"
+
+    "6. Jak często wypożyczasz książki w bibliotece"
+        "nigdy"
+        "raz w roku"
+        "raz na pol roku"
+        "raz na mięsiąc"
+
+    "7. Ile książek czytasz średnio w ciągu roku?"
+        "0"
+        "1"
+        "2 lub 3"
+        "4-10"
+
+
+
+
+
+
+
