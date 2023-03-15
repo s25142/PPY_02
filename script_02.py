a2e8d2313bf0387262fdfedb03937acb2518a2d5
s25142 <s25142@pjwstk.edu.pl>
2023-03-15 11:41:15 +0100

    Dodanie skryptu drugiego

---
+++ script_02.py
@@ -0,0 +1,14 @@
+num1 = float(input("Podaj pierwsza liczbe: "))
+num2 = float(input("Podaj druga liczbe: "))
+operator = input("podaj operator: ")
+
+# Napisz skrypt kalkulator, który pobierze od użytkownika 2 liczby a następnie po podaniu
+# odpowiedniego operatora wykona adekwatną operację i wyświetli wynik.
+
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+else:
+    print("nie ma takiego operatora")
