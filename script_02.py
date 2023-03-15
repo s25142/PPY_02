8a543d6df30ab3c0abdc6159441b3dcbcacc0af7
s25142 <s25142@pjwstk.edu.pl>
2023-03-15 21:42:03 +0100

    Dodanie rozwiązania zadania drugiego

--- script_02.py
+++ script_02.py
@@ -1,14 +1,34 @@
-num1 = float(input("Podaj pierwsza liczbe: "))
-num2 = float(input("Podaj druga liczbe: "))
-operator = input("podaj operator: ")
-
-# Napisz skrypt kalkulator, który pobierze od użytkownika 2 liczby a następnie po podaniu
-# odpowiedniego operatora wykona adekwatną operację i wyświetli wynik.
 
 
-if operator == "+":
-    print(num1 + num2)
-elif operator == "-":
-    print(num1 - num2)
-else:
-    print("nie ma takiego operatora")
+while bool(1):
+
+    num1 = float(input("Podaj pierwsza liczbe: "))
+    num2 = float(input("Podaj druga liczbe: "))
+    operator = input("podaj operator: ")
+
+    # Napisz skrypt kalkulator, który pobierze od użytkownika 2 liczby a następnie po podaniu
+    # odpowiedniego operatora wykona adekwatną operację i wyświetli wynik.
+
+
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "*":
+        print(num1 * num2)
+    elif operator == "/":
+        if(num2 == 0):
+            print("nie mozna dzielic przez 0")
+        else:
+            print(num1 / num2)
+    elif operator == "//":
+        if (num2 == 0):
+            print("nie mozna dzielic przez 0")
+        else:
+            print(num1 // num2)
+    elif operator == "%":
+            print(num1 % num2)
+    elif operator == "**":
+        print(num1 ** num2)
+    else:
+        print("nie ma takiego operatora", "\n")
